2908eb8100023e1a55492c8bd3bfdf54925525b0
Fermin <fermin.conejo@medianet.es>
2015-10-05 16:38:08 +0200

    tabs to spaces

--- app/src/cambio.js
+++ app/src/cambio.js
@@ -1,6 +1,6 @@
 function cambioDivisas(obj){
-	this.cambio = obj.cambio;
-	this.cambioDiv = function(){
-		return this.cambio.multiplicacion();
-	};
+    this.cambio = obj.cambio;
+    this.cambioDiv = function(){
+        return this.cambio.multiplicacion();
+    };
 }
--- app/src/operaciones.js
+++ app/src/operaciones.js
@@ -1,17 +1,17 @@
 function operaciones(n1,n2){
-	this.suma = function(){
-		return (n1 + n2).toFixed(2);
-	};
+    this.suma = function(){
+        return (n1 + n2).toFixed(2);
+    };
 
-	this.resta = function(){
-		return (n1 - n2).toFixed(2);
-	};
+    this.resta = function(){
+        return (n1 - n2).toFixed(2);
+    };
 
-	this.multiplicacion = function(){
-		return (n1 * n2).toFixed(2);
-	};
+    this.multiplicacion = function(){
+        return (n1 * n2).toFixed(2);
+    };
 
-	this.division = function(){
-		return (n1 / n2).toFixed(2);
-	};
+    this.division = function(){
+        return (n1 / n2).toFixed(2);
+    };
 }
